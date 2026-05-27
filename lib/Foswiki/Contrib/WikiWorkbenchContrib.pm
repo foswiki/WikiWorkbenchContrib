f0ddba0c3634a235b5c43e9e2e1b4f64c73692ba
MichaelDaum <daum@michaeldaumconsulting.com>
2026-05-27 12:45:52 +0200

    Item15669: remove unused service scripts

--- lib/Foswiki/Contrib/WikiWorkbenchContrib.pm
+++ lib/Foswiki/Contrib/WikiWorkbenchContrib.pm
@@ -1,6 +1,6 @@
 # Module of Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 #
-# Copyright (C) 2007-2025 Michael Daum http://michaeldaumconsulting.com
+# Copyright (C) 2007-2026 Michael Daum http://michaeldaumconsulting.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -21,33 +21,9 @@ use warnings;
 use Foswiki::Func();
 use Error qw(:try);
 
-our $VERSION = '6.20';
+our $VERSION = '6.30';
 our $RELEASE = '%$RELEASE%';
 our $SHORTDESCRIPTION = 'Framework for <nop>WikiApplications';
 our $LICENSECODE = '%$LICENSECODE%';
-
-sub convertTopicType {
-  my ($session) = @_;
-
-  my $request = Foswiki::Func::getRequestObject();
-  my $class = $request->param("class");
-
-  try {
-    throw Error::Simple("no class specified") unless defined $class;
-    $class = 'Foswiki::Contrib::WikiWorkbenchContrib::'.$class unless $class =~ /^Foswiki::/;
-
-    my $path = $class . '.pm';
-    $path =~ s/::/\//g;
-    eval {require $path};
-    throw Error::Simple($@) if $@;
-
-    my $impl = $class->new();
-    $impl->convert($session);
-  } catch Error with {
-    my $error = shift;
-
-    print STDERR "ERROR: ".$error."\n";
-  };
-}
 
 1;
